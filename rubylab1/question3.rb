0c83f8bf2875adf4a9a02d44b5ef84c944c225a3
Fletcher Richman <frichman3@gmail.com>
2014-01-15 23:41:13 -0700

    commented out tests for q3

--- rubylab1/question3.rb
+++ rubylab1/question3.rb
@@ -13,10 +13,10 @@ end
 
 # Example test cases:
 
-puts count_words("A man, a plan, a canal -- Panama")
+#puts count_words("A man, a plan, a canal -- Panama")
 
 #     # => {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
 
-puts count_words "Doo bee doo bee doo"
+#puts count_words "Doo bee doo bee doo"
 
 #     # => {'doo' => 3, 'bee' => 2}
