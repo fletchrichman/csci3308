d782c055163577f93c64ea7b30a0ea9caa7d50ff
Fletcher Richman <frichman3@gmail.com>
2014-02-01 15:57:28 -0700

    q9 done

--- rubylab1/question9.rb
+++ rubylab1/question9.rb
@@ -15,7 +15,7 @@
 
 class Numeric
 
- @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+ @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
 
  def method_missing(method_id)
 
@@ -33,6 +33,22 @@ class Numeric
 
  end
 
+ def in(currency)
+
+  currency = currency.to_s.gsub(/s$/, '')
+
+  if @@currencies.has_key?(currency)
+    self/@@currencies[currency]
+  else
+    super
+  end
+
+ end
+
 end
+
+p 5.dollars.in(:euros)
+p 10.euros.in(:rupees)
+
 
  
